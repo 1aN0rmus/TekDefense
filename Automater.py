1b2bcab135ebe84d1a1449287719a02c3e3bb658
1aN0rmus <ian.ahl@tekdefense.com>
2012-11-22 22:51:06 -0500

    Update Automater.py
    Added URL support.  A file with IPs and URLs will sort automatically.  URL cannot include HTTP:// right now.

--- Automater.py
+++ Automater.py
@@ -10,9 +10,9 @@ print '''
 | | | | |_| | || (_) | | | | | | (_| | ||  __/ |   
 \_| |_/\__,_|\__\___/|_| |_| |_|\__,_|\__\___|_|   
 
-Welcome to Automater! This script is used to list domains that an IP Address
-resolves too, and tell if the domain is blacklisted.  This script currently queries
-robtex.com and ipvoid.com for this info, but will include other sources in the future.
+Welcome to Automater! I have created this tool to help analyst investigate IP Addresses and URLs with the common web based tools.  All activity is passive so it will not alert attackers.
+Web Tools used are:
+IPvoid.com, Robtex.com, Fortiguard.com, unshorten.me, Urlvoid.com 
 www.TekDefense.com
 @author: 1aN0rmus@TekDefense.com
 '''
@@ -26,7 +26,7 @@ To do:
 -import list (Complete)
 -output to file (Complete)
 -Fix IPvoid for IP's that haven't been scanned previously. (Complete)
--Add URL support!!!
+-Add URL support (Complete)
 -Add command options/arguments (Complete, Yay Argparse!)
 -Add malwaredomainlist checker
 -unshorten url (Complete)
@@ -37,27 +37,12 @@ To do:
 -Pretty up output
 '''
 
-#urlInput = "tekdefense.com"
-def usage():
-    print '''
-    ONLY -t, -h, and -f WORK CURRENTLY!! 
-    -t: target ip or url.  URL must include http://
-    -s: source engine (robtex, ipvoid, fortiguard)
-    -a: all engines
-    -h: help
-    -f: import a file of IPs and/or URLs
-    -o: output results to file
-    -i: Interactive Mode
-    Examples:
-    ./Automater.py -t 123.123.123.123 -a -o result.txt
-    ./Automater.py -f hosts.txt -s robtex -o results.txt
-    '''  
-          
+#urlInput = "tekdefense.com"      
 #ipInput = (raw_input('Please enter an IP address to be queried: '))
 
 def main():   
     parser = argparse.ArgumentParser(description='This is Automater Biotch!')
-    parser.add_argument('-t', '--target', help='List one or more IP Addresses.  If more than one seperate with a space.')
+    parser.add_argument('-t', '--target', help='List one IP Addresses to query.  Does not support more than one address.')
     parser.add_argument('-f', '--file', help='This option is used to import a file that contains IP Addresses or URLs')
     parser.add_argument('-o', '--output', help='This option will output the results to a file.')
     parser.add_argument('-e', '--expand', help='This option will expand a shortened url using unshort.me')
@@ -69,10 +54,37 @@ def main():
             output = str(args.output)
             o = open(output, "w")
             sys.stdout = o  
-        ipInput = args.target
-        robtex(ipInput)
-        ipvoid(ipInput)
-        fortiURL(ipInput)
+        input = args.target
+        rpd7 = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', re.IGNORECASE)
+        rpdFind7 = re.findall(rpd7,input)
+        rpdSorted7=sorted(rpdFind7)
+        rpdSorted7=str(rpdSorted7)
+        rpdSorted7=rpdSorted7[2:-2]
+    
+        rpd8 = re.compile('([-a-z0-9A-Z]+\.[-a-z0-9A-Z]*).+', re.IGNORECASE)
+        rpdFind8 = re.findall(rpd8,input)
+        rpdSorted8=sorted(rpdFind8)
+        rpdSorted8=str(rpdSorted8)
+        rpdSorted8=rpdSorted8[2:-2]
+        if rpdSorted7 == input:
+            print '------------------------------'
+            print '------------------------------'
+            print input + ' is an IP.  Running IP toolset'
+            print '------------------------------'
+            print '------------------------------'
+            ipInput = input
+            robtex(ipInput)
+            ipvoid(ipInput)
+            fortiURL(ipInput)
+        else:
+            print '------------------------------'
+            print '------------------------------'
+            print input + ' is a URL.  Running URL toolset'
+            print '------------------------------'
+            print '------------------------------'
+            urlInput = input
+            unshortunURL(urlInput)
+            urlvoid(urlInput)
     elif args.file:
         if args.output != None:
             print 'Printing results to file:', args.output
@@ -84,8 +96,42 @@ def main():
         for i in li:
             li = str(i)
             ipInput = li.strip()
-            robtex(ipInput)
-            ipvoid(ipInput)
+            input = ipInput
+            rpd7 = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', re.IGNORECASE)
+            rpdFind7 = re.findall(rpd7,input)
+            rpdSorted7=sorted(rpdFind7)
+            rpdSorted7=str(rpdSorted7)
+            rpdSorted7=rpdSorted7[2:-2]
+    
+            rpd8 = re.compile('([-a-z0-9A-Z]+\.[-a-z0-9A-Z]*).+', re.IGNORECASE)
+            rpdFind8 = re.findall(rpd8,input)
+            rpdSorted8=sorted(rpdFind8)
+            rpdSorted8=str(rpdSorted8)
+            rpdSorted8=rpdSorted8[2:-2]
+            if rpdSorted7 == input:
+                print '------------------------------'
+                print '------------------------------'
+                print input + ' is an IP.  Running IP toolset'
+                print '------------------------------'
+                print '------------------------------'
+                ipInput = input
+                robtex(ipInput)
+                ipvoid(ipInput)
+                fortiURL(ipInput)
+                print ''
+                print ''
+            else:
+                print '------------------------------'
+                print '------------------------------'
+                print input + ' is a URL.  Running URL toolset'
+                print '------------------------------'
+                print '------------------------------'
+                urlInput = input
+                urlvoid(urlInput)
+                unshortunURL(urlInput)
+                print ''
+                print ''
+                
     elif args.expand:
         url = args.expand
         unshortunURL(url)
@@ -254,7 +300,86 @@ def unshortunURL(url):
             print (url + ' redirects to: ' + m)
         else:
             print (url + ' is not a recognized shortened URL.')
+def urlvoid(url):                
+    h2 = httplib2.Http(".cache")
+    resp, content2 = h2.request(("http://urlvoid.com/scan/" + url), "GET")
+    content2String = (str(content2))
+    rpderr = re.compile('\<div\sclass\=\"error\"\>', re.IGNORECASE)
+    rpdFinderr = re.findall(rpderr,content2String)
+    if "error" in str(rpdFinderr):
+        ipvoidErr = True
+    else:
+        ipvoidErr = False
+    if ipvoidErr == False:
+        
+        rpd1 = re.compile('\<td\>IP\sAddress:\<\/td\>\n\s+\<td\>\n\s+\<a\shref=\"\/ip\/(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).+\n\s+\<\/td>', re.IGNORECASE)
+        rpdFind1 = re.findall(rpd1,content2String)
+        rpdSorted1=sorted(rpdFind1) 
+        
+        rpd2 = re.compile('\>DETECTED\<span\>\<\/td\>\n\s+<td\>\<a\srel="nofollow"\shref="(\w+:\/\/.+)"\s', re.IGNORECASE)
+        rpdFind2 = re.findall(rpd2,content2String)
+        rpdSorted2=sorted(rpdFind2)   
+
+        rpd3 = re.compile('\<td\>ISP:\<\/td\>\n\s+\<td\>(.+)\<\/td>', re.IGNORECASE)
+        rpdFind3 = re.findall(rpd3,content2String)
+        rpdSorted3=sorted(rpdFind3)
+        
+        rpd4 = re.compile('\<td\>IP\sCountry:\<\/td\>\n\s+\<td\>.+\/\>\s(.+)\<\/td>', re.IGNORECASE)
+        rpdFind4 = re.findall(rpd4,content2String)
+        rpdSorted4=sorted(rpdFind4)
+        
+        rpd5 = re.compile('\<td\>Domain\sCreated:\<\/td\>\n\s+\<td\>(.+)\<\/td>', re.IGNORECASE)
+        rpdFind5 = re.findall(rpd5,content2String)
+        rpdSorted5=sorted(rpdFind5)
+        
+        print ''    
+        print 'IP Address:'
+        print '------------------------------' 
+        
+        i=''
+        for i in rpdSorted1:
+            print ('Host IP Address is '+ i)
+        if i=='':
+            print('IP is not listed')
+    
+        
+        print ''
+        print 'Blacklist Status:'
+        print '------------------------------' 
+        
+        j=''
+        for j in rpdSorted2:
+            print ('Host is listed in blacklist at '+ j)
+        if j=='':
+            print('IP is not listed in a blacklist')
+    
+        print ''    
+        print 'IP ISP and Geo Location:'
+        print '------------------------------' 
+       
+        k=''
+        for k in rpdSorted3:
+            print ('ISP: '+ k)
+        if k=='':
+            print('No ISP listed')
+        
+        l=''
+        for l in rpdSorted4:
+            print ('Geographic Location: '+ l)
+        if l=='':
+            print ('No GEO location listed')
+        
+        m=''
+        for m in rpdSorted5:
+            print ('Domain creation date: '+ m)
+        if m=='':
+            print ('Domain creation date not listed.')
+
+    
 '''
+(?:\d{1,3}\.){3}\d{1,3}|                    (?# IPv4 address)
+[:a-fA-F0-9]*:[:a-fA-F0-9]*:[:a-fA-F0-9.]*| (?# IPv6 address)
+                (?# domain name)
 http://unshort.me/index.php?r=bit.ly/XDlV1q
 74.125.232.102
 188.95.52.162
