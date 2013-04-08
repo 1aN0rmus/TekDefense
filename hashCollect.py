01942e86770abf90f0c63b6bcb0ac6662c85483f
1aN0rmus <ian.ahl@tekdefense.com>
2013-04-07 22:48:19 -0300

    Update hashCollect to allow custom regex
    Hash collect can now take custom regex with a -r. The regex value must be incased in single or double quotes.
    Example:
    hashCollect -f filename -r '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'

--- hashCollect.py
+++ hashCollect.py
@@ -12,7 +12,15 @@ parser = argparse.ArgumentParser(description='Hash Collector')
 parser.add_argument('-u', '--url', help='This option is used to search for hashes on a website')
 parser.add_argument('-f', '--file', help='This option is used to import a file that contains hashes')
 parser.add_argument('-o', '--output', help='This option will output the results to a file.')
+parser.add_argument('-r', '--regex', help='This option allows the user to set a custom regex value. Must incase in single or double quotes.')
+
+
 args = parser.parse_args()
+regVal = '[a-fA-F0-9]{32}'
+listResults = []
+
+if args.regex:
+    regVal = str(args.regex)
 
 if args.url == None and args.file == None:
     parser.print_help()
@@ -29,20 +37,25 @@ if args.file:
     strFile=''
     for line in fileImport:
         strFile += line    
-    regexValue = re.compile('[a-fA-F0-9]{32}')
+    regexValue = re.compile(regVal)
     regexSearch = re.findall(regexValue,strFile)
     for i in regexSearch:
-        print i
+        listResults.append(i)
     
 if args.url:
     url = args.url
     h = httplib2.Http(".cache")
     resp, content = h.request((url), "GET")
     contentString = (str(content))
-    regexValue = re.compile('[a-fA-F0-9]{32}')
+    regexValue = re.compile(regVal)
     regexSearch = re.findall(regexValue,contentString)
     for i in regexSearch:
-        print i
+        listResults.append(i)
+ 
+listResults = list(set(listResults))  
+for i in listResults:
+    print i
+          
 
 if __name__ == '__main__':
     pass
