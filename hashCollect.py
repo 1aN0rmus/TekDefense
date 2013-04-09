dfe3bd1b14ca28b0e92c4d0ee4f4f633b8437811
1aN0rmus <ian.ahl@tekdefense.com>
2013-04-09 12:15:41 -0300

    Update hashCollect.py
    Changelog:
    .4
    [+] Added summary function that will show number of each data type within a target URL or file
    .3
    [+] Added predefined data types that can be invoke with -t type
    .2
    [+] Expanded the script to allow custom regex with a -r 'regex here'
    .1
    [+] Tool created and can only pull md5 hashes

--- hashCollect.py
+++ hashCollect.py
@@ -5,9 +5,11 @@ This is tekCollect! This tool will scrape specified data types out of a URL or f
 @TekDefense
 Ian Ahl | www.TekDefense.com | 1aN0rmus@tekDefense.com
 *Some of the Regular Expressions were taken from http://gskinner.com/RegExr/
-Version: 0.3
+Version: 0.4
 
 Changelog:
+.4
+[+] Added summary function that will show number of each data type within a target URL or file
 .3
 [+] Added predefined data types that can be invoke with -t type
 .2
@@ -25,40 +27,52 @@ parser.add_argument('-f', '--file', help='This option is used to import a file t
 parser.add_argument('-o', '--output', help='This option will output the results to a file.')
 parser.add_argument('-r', '--regex', help='This option allows the user to set a custom regex value. Must incase in single or double quotes.')
 parser.add_argument('-t', '--type', help='This option allows a user to choose the type of data they want to pull out. Currently MD5, SHA1, SHA 256, Domain, URL, IP4, IP6, CCN, SSN, EMAIL')
+parser.add_argument('-s', '--Summary', action='store_true', default=False, help='This options will show a summary of the data types in a file')
 args = parser.parse_args()
 
-# Setting some variables ans lists 
+# Setting some variables and lists 
 regVal = '[a-fA-F0-9]{32}'
 listResults = []
-
+MD5 = '[a-fA-F0-9]{32}'
+SHA1 = '[a-fA-F0-9]{40}'
+SHA256 = '[a-fA-F0-9]{64}'
+LM = '[a-fA-F0-9]{32}'
+DOMAIN = '([a-zA-Z0-9-]+\.)(com|net|biz|cat|aero|asia|coop|info|int|jobs|mobi|museum|name|org|post|pre|tel|travel|xxx|edu|gov|mil|br|cc|ca|uk|ch|co|cx|de|fr|hk|jp|kr|nl|nr|ru|tk|ws|tw)'
+URL = '(http\:\/\/|https\:\/\/)([a-zA-Z0-9-]+\.)(com|net|biz|cat|aero|asia|coop|info|int|jobs|mobi|museum|name|org|post|pre|tel|travel|xxx|edu|gov|mil|br|cc|ca|uk|ch|co|cx|de|fr|hk|jp|kr|nl|nr|ru|tk|ws|tw)'
+IP4 = '((?<![0-9])(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))(?![0-9]))'
+IP6 = '(((([01]? d?\\d)|(2[0-5]{2}))\\.){3}(([01]?\\d?\\d)|(2[0-5]{2})))|(([A-F0-9]){4}(:|::)){1,7}(([A-F0-9]){4})'
+SSN = '(\d{3}\-\d{2}\-\d{3})|(\d{3}\s\d{2}\s\d{3})'
+EMAIL = '([a-zA-Z0-9\.-_]+@)([a-zA-Z0-9-]+\.)(com|net|biz|cat|aero|asia|coop|info|int|jobs|mobi|museum|name|org|post|pre|tel|travel|xxx|edu|gov|mil|br|cc|ca|uk|ch|co|cx|de|fr|hk|jp|kr|nl|nr|ru|tk|ws|tw)'
+CCN = '\d{14,16}|\d{4}\s\d{4}\s\d{4}\s\d{2,4}|\d{4}\-\d{4}\-\d{4}\-\d{2,4}'
+listTypes = [('MD5',MD5), ('SHA1',SHA1), ('SHA256',SHA256), ('DOMAIN', DOMAIN), ('URL', URL), ('IP4',IP4), ('IP6',IP6), ('SSN', SSN), ('EMAIL',EMAIL), ('CCN',CCN)]
 # Determining what type of data the user wants and setting the regex to the regVal variable for that data type 
 if args.type:
     if args.type.upper() == 'MD5':
-        regVal = '[a-fA-F0-9]{32}'
-    if args.type.upper() == 'SHA1':
-        regVal = '[a-fA-F0-9]{40}'
-    if args.type.upper() == 'SHA256':
-        regVal = '[a-fA-F0-9]{64}'
-    if args.type.upper() == 'LM':
-        regVal = '[a-fA-F0-9]{32}'
-    if args.type.upper() == 'DOMAIN':
-        regVal = '([a-zA-Z0-9-]+\.)(com|net|biz|cat|aero|asia|coop|info|int|jobs|mobi|museum|name|org|post|pre|tel|travel|xxx|edu|gov|mil|br|cc|ca|uk|ch|co|cx|de|fr|hk|jp|kr|nl|nr|ru|tk|ws|tw)'
-    if args.type == 'URL':
-        regVal = '(http\:\/\/|https\:\/\/)([a-zA-Z0-9-]+\.)(com|net|biz|cat|aero|asia|coop|info|int|jobs|mobi|museum|name|org|post|pre|tel|travel|xxx|edu|gov|mil|br|cc|ca|uk|ch|co|cx|de|fr|hk|jp|kr|nl|nr|ru|tk|ws|tw)'
-    if args.type.upper() == 'IP4':
-        regVal = '((?<![0-9])(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))(?![0-9]))'
-    if args.type.upper() == 'IP6':
-        regVal = '(((([01]? d?\\d)|(2[0-5]{2}))\\.){3}(([01]?\\d?\\d)|(2[0-5]{2})))|(([A-F0-9]){4}(:|::)){1,7}(([A-F0-9]){4})'
-    if args.type.upper() == 'SSN':
-        regVal = '(\d{3}\-\d{2}\-\d{3})|(\d{3}\s\d{2}\s\d{3})'
-    if args.type.upper() == 'EMAIL':
-        regVal = '([a-zA-Z0-9\.-_]+@)([a-zA-Z0-9-]+\.)(com|net|biz|cat|aero|asia|coop|info|int|jobs|mobi|museum|name|org|post|pre|tel|travel|xxx|edu|gov|mil|br|cc|ca|uk|ch|co|cx|de|fr|hk|jp|kr|nl|nr|ru|tk|ws|tw)'
-    if args.type.upper() == 'CCN':
-        regVal = '\d{14,16}|\d{4}\s\d{4}\s\d{4}\s\d{2,4}|\d{4}\-\d{4}\-\d{4}\-\d{2,4}'
+        regVal = MD5
+    elif args.type.upper() == 'SHA1':
+        regVal = SHA1
+    elif args.type.upper() == 'SHA256':
+        regVal = SHA256
+    elif args.type.upper() == 'LM':
+        regVal = LM
+    elif args.type.upper() == 'DOMAIN':
+        regVal = DOMAIN
+    elif args.type == 'URL':
+        regVal = IP4
+    elif args.type.upper() == 'IP4':
+        regVal = URL
+    elif args.type.upper() == 'IP6':
+        regVal = IP6
+    elif args.type.upper() == 'SSN':
+        regVal = SSN
+    elif args.type.upper() == 'EMAIL':
+        regVal = EMAIL
+    elif args.type.upper() == 'CCN':
+        regVal = CCN
     # If the user puts in a data type we do not have defined above, then let them know what types of data are available.
-    #else:
-        #print '[-] ' + args.type + ' is not a valid type. Current valid types are MD5, SHA1, SHA256, DOMAIN, URL, IP4, IP6, SSN, EMAIL, and CCN'
-        #sys.exit()
+    else:
+        print '[-] ' + args.type + ' is not a valid type. Current valid types are MD5, SHA1, SHA256, DOMAIN, URL, IP4, IP6, SSN, EMAIL, and CCN'
+        sys.exit()
 # If the user wants to set a custom regex, it is collected here and added to the regVal variable.
 if args.regex:
     regVal = str(args.regex)
@@ -77,32 +91,75 @@ if args.output:
 
 # If the target to scrape is a file open the file create a string for each line, regex the string for the data type specified by the regVal, and put results in a list.
 if args.file:
-    iFile = args.file
-    fileImport =open(iFile)
-    strFile=''
-    for line in fileImport:
-        strFile += line    
-    regexValue = re.compile(regVal)
-    regexSearch = re.findall(regexValue,strFile)
-    for i in regexSearch:
-        listResults.append(i)
+    if args.Summary == True:
+        iFile = args.file
+        fileImport =open(iFile)
+        strFile=''
+        for line in fileImport:
+            strFile += line
+        for i in listTypes:
+            regVal = i[1]
+            regexValue = re.compile(regVal)
+            regexSearch = re.findall(regexValue,strFile)
+            listResults = []
+            for j in regexSearch:
+                listResults.append(j)
+            #for i in tup in 
+            listResults = list(set(listResults)) 
+            for k in listResults:
+                ''.join(k) 
+            print 'There number of ' + i[0] + ' in the target: ' + str(len(listResults))
+        sys.exit()  
+    else:
+        iFile = args.file
+        fileImport =open(iFile)
+        strFile=''
+        for line in fileImport:
+            strFile += line    
+        regexValue = re.compile(regVal)
+        regexSearch = re.findall(regexValue,strFile)
+        for i in regexSearch:
+            listResults.append(i)
 
 # If the target to scrape is a url conect to and get content from the url, create a string out of the content, regex the string for the data type specified by the regVal, and put results in a list.    
 if args.url:
-    url = args.url
-    h = httplib2.Http(".cache")
-    resp, content = h.request((url), "GET")
-    contentString = (str(content))
-    regexValue = re.compile(regVal)
-    regexSearch = re.findall(regexValue,contentString)
-    for i in regexSearch:
-        listResults.append(i)
+    if args.Summary == True:
+        url = args.url
+        h = httplib2.Http(".cache")
+        resp, content = h.request((url), "GET")
+        contentString = (str(content))
+        for i in listTypes:
+            regVal = i[1]
+            regexValue = re.compile(regVal)
+            regexSearch = re.findall(regexValue,contentString)
+            listResults = []
+            for j in regexSearch:
+                listResults.append(j)
+            #for i in tup in 
+            listResults = list(set(listResults)) 
+            for k in listResults:
+                ''.join(k) 
+            print 'There number of ' + i[0] + ' in the target: ' + str(len(listResults))
+        sys.exit()
+    else:
+        url = args.url
+        h = httplib2.Http(".cache")
+        resp, content = h.request((url), "GET")
+        contentString = (str(content))
+        regexValue = re.compile(regVal)
+        regexSearch = re.findall(regexValue,contentString)
+        for i in regexSearch:
+            listResults.append(i)
 
 # Remove duplicates from the list and print
 listResults = list(set(listResults))  
 for i in listResults:
     print ''.join(i)
-          
+
+def summary(listTypes):
+    for i in listTypes:
+        regVal = i
+                   
 
 if __name__ == '__main__':
     pass
