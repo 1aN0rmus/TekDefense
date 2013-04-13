26a00129dd3c6f9e159800e6be95b9b48e862c42
1aN0rmus <ian.ahl@tekdefense.com>
2013-04-13 00:16:44 -0300

    Update Automater.py
    1.2.3
    [+] Added HTTP Proxy support. Will pull OS default proxy settings.
    [+] Modified some variables for consistency 
    [+] Added comments
    [-] Removed JoeBox from hash search

--- Automater.py
+++ Automater.py
@@ -14,10 +14,15 @@ Welcome to Automater! I have created this tool to help analyst investigate IP Ad
 Web Tools used are: IPvoid.com, Robtex.com, Fortiguard.com, unshorten.me, Urlvoid.com, Labs.alienvault.com
 www.TekDefense.com
 @author: 1aN0rmus@TekDefense.com, Ian Ahl, @TekDefense
-Version 1.2.2
+Version 1.2.3
 '''
 '''
 Changelog:
+1.2.3
+[+] Added HTTP Proxy support. Will pull OS default proxy settings.
+[+] Modified some variables for consistency 
+[+] Added comments
+[-] Removed JoeBox from hash search
 1.2.2
 [+] Fixed FortiGuard rating https://github.com/1aN0rmus/TekDefense/issues/10
 [+] Display help when no arguments are given https://github.com/1aN0rmus/TekDefense/issues/8
@@ -31,21 +36,21 @@ Changelog:
 [+] Fixed IPVoid and URLVoid result for new regexes
 [+] Fixed form submit for IP's and URLs that were not previously scanned
 '''
-# md5 = '513f8915be522be98a4124d6958391f9'
-#urlInput = "tekdefense.com"      
-#ipInput = (raw_input('Please enter an IP address to be queried: '))
 
 def main():   
-    parser = argparse.ArgumentParser(description='IP and URL Passive Analysis tool')
-    parser.add_argument('-t', '--target', help='List one IP Addresses to query.  Does not support more than one address.')
-    parser.add_argument('-f', '--file', help='This option is used to import a file that contains IP Addresses or URLs')
+    # Adding arguments
+    parser = argparse.ArgumentParser(description='IP, URL, and Hash Passive Analysis tool')
+    parser.add_argument('-t', '--target', help='List one IP Addresses, URL or Hash to query.  Does not support more than one address.')
+    parser.add_argument('-f', '--file', help='This option is used to import a file that contains IP Addresses, URLs or Hashes')
     parser.add_argument('-o', '--output', help='This option will output the results to a file.')
     parser.add_argument('-e', '--expand', help='This option will expand a shortened url using unshort.me')
-    parser.add_argument('-s', '--source', help='This option will only run the target against a specifc source engine to pull associated domains.  Options are robtex, ipvoid, fortinet, urlvoid, alienvault')
+    parser.add_argument('-s', '--source', help='This option will only run the target against a specific source engine to pull associated domains.  Options are robtex, ipvoid, fortinet, urlvoid, alienvault')
     args = parser.parse_args()
+    # If no -t or -f is given show the help
     if args.target == None and args.file == None:
         parser.print_help()
         sys.exit(1)
+    # Options to run individual source engines
     if args.source == "robtex":
             ipInput = str(args.target)
             print args.source + " source engine selected"
@@ -111,13 +116,14 @@ def main():
         unshortunURL(url)
 
 def robtex(ipInput):   
-    h1 = httplib2.Http(".cache")
-    resp, content1 = h1.request(("http://robtex.com/" + ipInput), "GET")
-    content1String = (str(content1))
-    #print content1String
+    proxy = urllib2.ProxyHandler()
+    opener = urllib2.build_opener(proxy)
+    response = opener.open("http://robtex.com/" + ipInput)
+    content = response.read()
+    contentString = str(content)
 
     rpd = re.compile('href\=\"\/\/.+\.robtex\.com\/(.+).html\"\s+\>.+\<\/a\>\s\<\/span\>\<\/td\>\n\<td\sclass\="\w+\"\scolspan\="\d*\"\>a', re.IGNORECASE)
-    rpdFind = re.findall(rpd,content1String)
+    rpdFind = re.findall(rpd,contentString)
     
     rpdSorted=sorted(rpdFind)
     
@@ -131,11 +137,14 @@ def robtex(ipInput):
     
     
 def ipvoid(ipInput):                
-    h2 = httplib2.Http(".cache")
-    resp, content2 = h2.request(("http://ipvoid.com/scan/" + ipInput), "GET")
-    content2String = (str(content2))
+    proxy = urllib2.ProxyHandler()
+    opener = urllib2.build_opener(proxy)
+    response = opener.open("http://ipvoid.com/scan/" + ipInput)
+    content = response.read()
+    contentString = str(content)
+    
     rpderr = re.compile('An\sError\soccurred', re.IGNORECASE)
-    rpdFinderr = re.findall(rpderr,content2String)
+    rpdFinderr = re.findall(rpderr,contentString)
     # print content2String
     if "ERROR" in str(rpdFinderr):
         ipvoidErr = True
@@ -143,15 +152,15 @@ def ipvoid(ipInput):
         ipvoidErr = False
     if ipvoidErr == False:
         rpd2 = re.compile('Detected\<\/font\>\<\/td..td..a.rel..nofollow..href.\"(.{6,70})\"\stitle\=\"View', re.IGNORECASE)
-        rpdFind2 = re.findall(rpd2,content2String)
+        rpdFind2 = re.findall(rpd2,contentString)
         rpdSorted2=sorted(rpdFind2)
     
         rpd3 = re.compile('ISP\<\/td\>\<td\>(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind3 = re.findall(rpd3,content2String)
+        rpdFind3 = re.findall(rpd3,contentString)
         rpdSorted3=sorted(rpdFind3)
     
         rpd4 = re.compile('Country\sCode.+flag\"\s\/\>\s(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind4 = re.findall(rpd4,content2String)
+        rpdFind4 = re.findall(rpd4,contentString)
         rpdSorted4=sorted(rpdFind4)
 
     
@@ -181,18 +190,18 @@ def ipvoid(ipInput):
         request = urllib2.Request(url,params,headers={'Content-type':'application/x-www-form-urlencoded'})
         page = urllib2.urlopen(request)
         page = page.read()
-        content2String = str(page)
+        contentString = str(page)
         
         rpd2 = re.compile('Detected\<\/font\>\<\/td..td..a.rel..nofollow..href.\"(.{6,70})\"\stitle\=\"View', re.IGNORECASE)
-        rpdFind2 = re.findall(rpd2,content2String)
+        rpdFind2 = re.findall(rpd2,contentString)
         rpdSorted2=sorted(rpdFind2)
     
         rpd3 = re.compile('ISP\<\/td\>\<td\>(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind3 = re.findall(rpd3,content2String)
+        rpdFind3 = re.findall(rpd3,contentString)
         rpdSorted3=sorted(rpdFind3)
     
         rpd4 = re.compile('Country\sCode.+flag\"\s\/\>\s(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind4 = re.findall(rpd4,content2String)
+        rpdFind4 = re.findall(rpd4,contentString)
         rpdSorted4=sorted(rpdFind4)
 
     
@@ -214,9 +223,11 @@ def ipvoid(ipInput):
         if l=='':
             print ('[-] No GEO location listed')
 def fortiURL(ipInput):
-    h = httplib2.Http(".cache")
-    resp, content = h.request(("http://www.fortiguard.com/ip_rep/index.php?data=" + ipInput + "&lookup=Lookup"), "GET")
-    contentString = (str(content))
+    proxy = urllib2.ProxyHandler()
+    opener = urllib2.build_opener(proxy)
+    response = opener.open("http://www.fortiguard.com/ip_rep/index.php?data=" + ipInput + "&lookup=Lookup")
+    content = response.read()
+    contentString = str(content)
     
     rpd = re.compile('Category:\s(.+)\<\/h3\>\s\<a', re.IGNORECASE)
     rpdFind = re.findall(rpd,contentString)
@@ -230,29 +241,34 @@ def fortiURL(ipInput):
         print ('[-] Unable to connect to FortiGuard.com')
 
 def unshortunURL(url):
-    h4 = httplib2.Http(".cache")
-    resp, content4 = h4.request(("http://unshort.me/index.php?r=" + url), "GET")
-    content4String = (str(content4))
+    proxy = urllib2.ProxyHandler()
+    opener = urllib2.build_opener(proxy)
+    response = opener.open("http://unshort.me/index.php?r=" + url)
+    content = response.read()
+    contentString = str(content)
     
-    rpd6 = re.compile('result\"\>\s\<a\shref\=\".+\>(.+)\<\/a\>\s', re.IGNORECASE)
-    rpdFind6 = re.findall(rpd6,content4String)
-    rpdSorted6=sorted(rpdFind6)
+    rpd = re.compile('result\"\>\s\<a\shref\=\".+\>(.+)\<\/a\>\s', re.IGNORECASE)
+    rpdFind = re.findall(rpd,contentString)
+    rpdSorted=sorted(rpdFind)
     
     # print content3String
 
     m=''
-    for m in rpdSorted6:
+    for m in rpdSorted:
         if url not in m:
             print ('[+] ' + url + ' redirects to: ' + m)
         else:
             print ('[-] ' + url + ' is not a recognized shortened URL.')
 def urlvoid(url):                
-    h2 = httplib2.Http(".cache")
-    resp, content2 = h2.request(("http://urlvoid.com/scan/" + url), "GET")
-    content2String = (str(content2))
+    proxy = urllib2.ProxyHandler()
+    opener = urllib2.build_opener(proxy)
+    response = opener.open("http://urlvoid.com/scan/" + url)
+    content = response.read()
+    contentString = str(content)
+    
     rpderr = re.compile('An\sError\soccurred', re.IGNORECASE)
-    rpdFinderr = re.findall(rpderr,content2String)
-    # print content2String
+    rpdFinderr = re.findall(rpderr,contentString)
+    # print contentString
     if "ERROR" in str(rpdFinderr):
         ipvoidErr = True
     else:
@@ -260,23 +276,23 @@ def urlvoid(url):
     if ipvoidErr == False:
         
         rpd1 = re.compile('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).+Scan\swith\s', re.IGNORECASE)
-        rpdFind1 = re.findall(rpd1,content2String)
+        rpdFind1 = re.findall(rpd1,contentString)
         rpdSorted1=sorted(rpdFind1) 
         
         rpd2 = re.compile('DETECTED.{25,40}href\=\"(.{10,50})\"\stitle', re.IGNORECASE)
-        rpdFind2 = re.findall(rpd2,content2String)
+        rpdFind2 = re.findall(rpd2,contentString)
         rpdSorted2=sorted(rpdFind2)   
 
         rpd3 = re.compile('latitude\s\/\slongitude.+\<td\>(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind3 = re.findall(rpd3,content2String)
+        rpdFind3 = re.findall(rpd3,contentString)
         rpdSorted3=sorted(rpdFind3)
         
         rpd4 = re.compile('alt\=\"flag\".+\>(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind4 = re.findall(rpd4,content2String)
+        rpdFind4 = re.findall(rpd4,contentString)
         rpdSorted4=sorted(rpdFind4)
         
         rpd5 = re.compile('Domain\s1st\sRegistered.+\<td\>(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind5 = re.findall(rpd5,content2String)
+        rpdFind5 = re.findall(rpd5,contentString)
         rpdSorted5=sorted(rpdFind5)
         
         i=''
@@ -316,26 +332,26 @@ def urlvoid(url):
         request = urllib2.Request(urlvoid,params,headers={'Content-type':'application/x-www-form-urlencoded'})
         page = urllib2.urlopen(request)
         page = page.read()
-        content2String = str(page)
-        #print content2String
+        contentString = str(page)
+        #print contentString
         rpd1 = re.compile('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).+Scan\swith\s', re.IGNORECASE)
-        rpdFind1 = re.findall(rpd1,content2String)
+        rpdFind1 = re.findall(rpd1,contentString)
         rpdSorted1=sorted(rpdFind1) 
         
         rpd2 = re.compile('DETECTED.{25,40}href\=\"(.{10,50})\"\stitle', re.IGNORECASE)
-        rpdFind2 = re.findall(rpd2,content2String)
+        rpdFind2 = re.findall(rpd2,contentString)
         rpdSorted2=sorted(rpdFind2)   
 
         rpd3 = re.compile('latitude\s\/\slongitude.+\<td\>(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind3 = re.findall(rpd3,content2String)
+        rpdFind3 = re.findall(rpd3,contentString)
         rpdSorted3=sorted(rpdFind3)
         
         rpd4 = re.compile('alt\=\"flag\".+\>(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind4 = re.findall(rpd4,content2String)
+        rpdFind4 = re.findall(rpd4,contentString)
         rpdSorted4=sorted(rpdFind4)
         
         rpd5 = re.compile('Domain\s1st\sRegistered.+\<td\>(.+)\<\/td\>', re.IGNORECASE)
-        rpdFind5 = re.findall(rpd5,content2String)
+        rpdFind5 = re.findall(rpd5,contentString)
         rpdSorted5=sorted(rpdFind5)
         
         i=''
@@ -369,13 +385,15 @@ def urlvoid(url):
             print ('[-] Domain creation date not listed.')
 
 def alienvault(ipInput):
-    h1 = httplib2.Http(".cache")
-    url = "http://labs.alienvault.com/labs/index.php/projects/open-source-ip-reputation-portal/information-about-ip/?ip=" + ipInput 
-    resp, conten1 = h1.request((url), "GET")
-    content1String = (str(conten1))
+    url = "http://labs.alienvault.com/labs/index.php/projects/open-source-ip-reputation-portal/information-about-ip/?ip=" + ipInput   
+    proxy = urllib2.ProxyHandler()
+    opener = urllib2.build_opener(proxy)
+    response = opener.open("http://labs.alienvault.com/labs/index.php/projects/open-source-ip-reputation-portal/information-about-ip/?ip=" + ipInput)
+    content = response.read()
+    contentString = str(content)
     
     rpd = re.compile('.*IP not found.*')
-    rpdFind = re.findall(rpd,content1String)
+    rpdFind = re.findall(rpd,contentString)
 
     if not rpdFind:
         print ('[+] IP is listed in AlienVault IP reputation database at ' + url)
@@ -383,38 +401,35 @@ def alienvault(ipInput):
         print ('[-] IP is not listed in AlienVault IP reputation database')
 
 def md5Hash(md5):
-    h = httplib2.Http(".cache")
-    url = "http://www.threatexpert.com/report.aspx?md5=" + md5 
-    resp, content = h.request((url), "GET")
-    contentString = (str(content))
-    #print contentString
+    # Set proxy based on system default
+    proxy = urllib2.ProxyHandler()
+    opener = urllib2.build_opener(proxy)
+    
+    # Connect to threatexpert and check if hash is listed 
+    url = "http://www.threatexpert.com/report.aspx?md5=" + md5
+    response = opener.open(url)
+    content = response.read()
+    contentString = str(content)
     rpd = re.compile('Submission\sreceived.\s(.+)\<\/li\>')
     rpdFind = re.findall(rpd,contentString)
 
-    h1 = httplib2.Http(".cache")
+    # Connect to minotaur and check if hash is listed 
     url1 = "http://minotauranalysis.com/search.aspx?q=" + md5 
-    resp, content1 = h1.request((url1), "GET")
-    contentString1 = (str(content1))
-    #print contentString1
+    response1 = opener.open(url1)
+    content1 = response1.read()
+    contentString1 = str(content1)
     rpd1 = re.compile('Date\sSubmitted.\<\/td\>\<td\>(.{12,25})\<\/td\>')
     rpdFind1 = re.findall(rpd1,contentString1)
-    
-    h2 = httplib2.Http(".cache")
-    url2 = "http://www.joesecurity.org/reports/report-" + md5 + '.html'
-    resp, content2 = h2.request((url2), "GET")
-    contentString2 = (str(content2))
-    #print contentString2
-    rpd2 = re.compile('404\s\-\s')
-    rpdFind2 = re.findall(rpd2,contentString2)
-    
-    h3 = httplib2.Http(".cache")
+
+    # Connect to vxvault and check if hash is listed 
     url3 = "http://vxvault.siri-urz.net/ViriList.php?MD5=" + md5
-    resp, content3 = h3.request((url3), "GET")
-    contentString3 = (str(content3))
-    #print contentString3
+    response3 = opener.open(url3)
+    content3 = response3.read()
+    contentString3 = str(content3)      
     rpd3 = re.compile('\d{4}\-\d{2}\-\d{2}')
     rpdFind3 = re.findall(rpd3,contentString3)
     
+    # print results of hash findings
     if rpdFind:
         print ('[+] MD5 last scanned on ' + str(rpdFind)[2:-2] + ' at ' + url)
     else:
@@ -425,16 +440,11 @@ def md5Hash(md5):
     else:
         print ('[-] MD5 Not Found on Minotaur')     
     
-    if rpdFind2:
-        print ('[-] MD5 not found at Joe SandBox')
-    else:
-        print ('[+] MD5 was seen at ' + url2)
-    
     if rpdFind3:
         print ('[+] MD5 last scanned on ' + str(rpdFind3[0]) + ' at ' + url3)
     else:
         print ('[-] MD5 Not Found on VxVault')   
-          
+# Determine if a target is an IP, URL, or HASH, then run the appropriate toolset          
 def targetID(input):
     rpd7 = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', re.IGNORECASE)
     rpdFind7 = re.findall(rpd7,input)
